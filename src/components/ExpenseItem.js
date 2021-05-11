7c200a4dc4ee2600175091bbec4e5fc6fe1b1ca1
Aaron Mayhew <standingbytocopy@gmail.com>
2021-05-11 13:08:12 -0500

    made date a separate component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 // Data = Props
 
 function ExpenseItem(data) {
-    // Date object using vanilla JS, fake dynamic for now, not needed now due to dynamic setup with props aka data
-    // const expenseDate = new Date(2888, 2, 28);
-    // const expenseTitle = 'Asset Insurance';
-    // const expenseAmount = 10000.00;
-
-    const month = data.date.toLocaleString('en-US', { month: 'long' });
-    const day = data.date.toLocaleString('en-US', { day: '2-digit' });
-    const year = data.date.getFullYear();
 
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={data.date} />
             <div className="expense-item__description">
                 <h2>{data.title}</h2>
                 <div className="expense-item__price">${data.amount}</div>
