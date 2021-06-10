3303ca8a35e3779a5af29089c00dbee454892e6b
Aaron Mayhew <standingbytocopy@gmail.com>
2021-06-09 19:42:59 -0500

    updated functions

--- src/components/NewAmount/AmountForm.js
+++ src/components/NewAmount/AmountForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import '../css/AmountForm.css'
 
 
-const AmountForm = () => {
+const AmountForm = (data) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
@@ -49,13 +49,13 @@ const AmountForm = () => {
         // prevents the default of the request being sent staying on the currently loaded page
         event.preventDefault();
 
-        const expenseData = {
+        const amountData = {
             title: enteredTitle,
             amount: enteredAmount,
             date: new Date(enteredDate)
         };
 
-        console.log(expenseData)
+        data.onSaveAmountData();
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
@@ -66,19 +66,29 @@ const AmountForm = () => {
             <div className="new-amount__controls">
                 <div className="new-amount__control">
                     <label className="t">Title</label>
-                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+                    <input type="text" 
+                    value={enteredTitle} 
+                    onChange={titleChangeHandler} />
                 </div>
                 <div className="new-amount__control">
                     <label className="t">Amount</label>
-                    <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input type="number" 
+                    value={enteredAmount} 
+                    min="0.01" 
+                    step="0.01" 
+                    onChange={amountChangeHandler} />
                 </div>
                 <div className="new-amount__control">
                     <label className="t">Date</label>
-                    <input type="date" value={enteredDate} min="2000-01-04" max="3000-01-04" onChange={dateChangeHandler} />
+                    <input type="date" 
+                    value={enteredDate} 
+                    min="2000-01-04" 
+                    max="3000-01-04" 
+                    onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className="new-amount__actions">
-                <button type="submit">Add Amount</button>
+                <button type="submit">Update</button>
             </div>
         </form>
     );
--- src/components/NewAmount/NewAmount.js
+++ src/components/NewAmount/NewAmount.js
@@ -3,11 +3,22 @@ import React from 'react'; //jsx under the hood uses this library
 import '../css/NewAmount.css'
 import AmountForm from './AmountForm';
 const NewAmount = () => {
+
+    
+    const saveAmountDataHandler = (enteredExpenseData) => {
+        const amountData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        console.log(amountData);
+    };
+    
+
     return (
         <div className="new-amount">
-            <AmountForm />
+            <AmountForm onSaveAmountData={saveAmountDataHandler} />
         </div>
-    )
+    );
 };
 
 export default NewAmount;
