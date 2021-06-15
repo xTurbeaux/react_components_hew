9a6bceb5a49201f1ababdc5dc6720be1b32db5bf
Aaron Mayhew <standingbytocopy@gmail.com>
2021-06-15 14:03:31 -0500

    cleaned up unused jsx

--- src/components/totals/ExpenseItem.js
+++ src/components/totals/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../ui/Card';
 import '../css/ExpenseItem.css';
@@ -9,22 +9,22 @@ const ExpenseAmount = (data) => {
     // react hook for functions, hooks start with use in their name
 
     // first element is a pointer to the actual value (title), second is the function to change the title (setTitle)
-   const [amount, setAmount] = useState(data.amount);
+//    const [amount, setAmount] = useState(data.amount);
 
    // using const for useState()
 
-    const clickHandler = () => {
-        setAmount('new amount');
-    };
+    // const clickHandler = () => {
+    //     setAmount('new amount');
+    // };
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={data.date} />
             <div className="expense-item__description">
                 <h2>{data.title}</h2>
-                <div className="expense-item__price">${amount}</div>
+                <div className="expense-item__price">${data.amount}</div>
             </div>
-            <button onClick={clickHandler}>Edit Amount</button>
+            {/* <button onClick={clickHandler}>Edit Amount</button> */}
         </Card>
     );
 }
