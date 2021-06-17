296949b4ef1ddf3f4f15e6dcd108f41faaf565bc
Aaron Mayhew <standingbytocopy@gmail.com>
2021-06-17 13:11:53 -0500

    added conditional for messages when no content is available

--- src/components/totals/Amounts.js
+++ src/components/totals/Amounts.js
@@ -19,38 +19,24 @@ const Amounts = (data) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
+    let expensesContent = <p>No Expenses Found</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem 
+            key={expense.id}
+            title={expense.title} 
+            amount={expense.amount} 
+            date={expense.date} 
+        />
+        ))
+    }
+
     return (
         <div>
             <Card className="amounts">
                 <Filter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem 
-                        key={expense.id}
-                        title={expense.title} 
-                        amount={expense.amount} 
-                        date={expense.date} 
-                    />
-                ))}
-                {/* <ExpenseItem 
-                title={data.items[0].title}
-                amount={data.items[0].amount}
-                date={data.items[0].date}  
-                />
-                <ExpenseItem 
-                title={data.items[1].title}
-                amount={data.items[1].amount}
-                date={data.items[1].date}  
-                />
-                <ExpenseItem 
-                title={data.items[2].title}
-                amount={data.items[2].amount}
-                date={data.items[2].date}  
-                />
-                <ExpenseItem 
-                title={data.items[3].title}
-                amount={data.items[3].amount}
-                date={data.items[3].date}  
-                /> */}
+                {expensesContent}
             </Card>
         </div>
     );
